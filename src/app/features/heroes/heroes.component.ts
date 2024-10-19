32c93105c224065fbb80584e634b60fa2892d4a5
Tomas Balducchi <tomasbalducchi@gmail.com>
2024-10-19 09:03:45 -0300

    create basic layout

--- src/app/features/heroes/heroes.component.ts
+++ src/app/features/heroes/heroes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { HeroesFilterComponent } from '../../shared/components/heroes-filter/heroes-filter.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, HeroesFilterComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
