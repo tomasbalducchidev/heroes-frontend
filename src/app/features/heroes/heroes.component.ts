d6165bc00282dfc2c5fe7ad925852549c1768eb9
Tomas Balducchi <tomasbalducchi@gmail.com>
2024-10-20 10:49:51 -0300

    unsubscribe from getAllHeroes since there's no http to automatically end the data stream

--- src/app/features/heroes/heroes.component.ts
+++ src/app/features/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { HeroesFilterComponent } from '../../shared/components/heroes-filter/heroes-filter.component';
 import { MatTableModule } from '@angular/material/table';
@@ -10,7 +10,7 @@ import { FormComponent } from './components/form/form.component';
 import { MessageComponent } from '../../shared/components/message/message.component';
 import { Router } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
-import { retry } from 'rxjs';
+import { retry, Subscription } from 'rxjs';
 
 
 @Component({
@@ -21,11 +21,14 @@ import { retry } from 'rxjs';
   styleUrl: './heroes.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit, OnDestroy {
+
 
   private router = inject(Router);
   readonly dialog = inject(MatDialog);
   private _heroesService = inject(HeroesService);
+  private subscriptions: Subscription = new Subscription();
+
 
   displayedColumns: string[] = ['id', 'name', 'actions'];
 
@@ -36,9 +39,15 @@ export class HeroesComponent implements OnInit {
 
   }
 
+  ngOnDestroy(): void {
+    if (this.subscriptions) {
+      this.subscriptions.unsubscribe();
+    }
+  }
+
   getHeroes = () => {
     try {
-      this._heroesService.getAllHeroes().pipe(
+      this.subscriptions = this._heroesService.getAllHeroes().pipe(
         retry(3)
       ).subscribe((heroes) => {
         this.heroes = heroes;
